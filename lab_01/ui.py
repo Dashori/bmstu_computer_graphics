772ffb62ee8dfbbe8a91f24d95241f36e62f1ef6
Dashori <daahaaa@icloud.com>
2022-02-10 23:07:31 +0300

    add all functions to work with ui(delete, add, change and clean

--- lab_01/ui.py
+++ lab_01/ui.py
@@ -4,9 +4,7 @@
 # множества минимальна.
 
 from tkinter import *
-
-from click import command
-import main
+import ui_func
 
 window=Tk()
 window.title('Lab 1')
@@ -34,7 +32,7 @@ count_points_label.place(x=50, y=110)
 input_count=Entry(font='Helvetica')
 input_count.place(x=370, y=107, width=50)
 
-count_points_button=Button(text='Ввод',font='Helvetica', command = lambda: main.check_count(input_count.get()))
+count_points_button=Button(text='Ввод',font='Helvetica', command = lambda: ui_func.check_count(input_count.get()))
 count_points_button.place(x=430, y=107, width=55, height=25)
 
 
@@ -66,7 +64,7 @@ for i in range(10):
     name_entry_y[i].place(x=150, y=198+i*30, width=50)
     name_entry_y[i].config(state='readonly')
    
-input_table_button=Button(font='Helvetica', text='Добавить', command=lambda: main.read_points(input_count.get()))
+input_table_button=Button(font='Helvetica', text='Добавить', command=lambda: ui_func.read_points(input_count.get()))
 input_table_button.place(x=220, y=468, width=80, height=27) 
 input_table_button.config(state='disable')
 
@@ -74,7 +72,7 @@ input_table_button.config(state='disable')
 ## Вывод таблицы точек
 ##
 
-view_table_button=Button(font='Helvetica 12 bold', text='Вывести таблицу', command = lambda: main.print_table())
+view_table_button=Button(font='Helvetica 12 bold', text='Вывести таблицу', command = lambda: ui_func.print_table())
 view_table_button.place(x=50, y=520)
 
 ##
@@ -93,7 +91,7 @@ add_point_entry_x.place(x=440, y=177, width=50)
 add_point_entry_y=Entry(font='Helvetica')
 add_point_entry_y.place(x=550, y=177, width=50)
 
-add_point_button=Button(font='Helvetica', text='Добавить', command=lambda: main.add_point())
+add_point_button=Button(font='Helvetica', text='Добавить', command=lambda: ui_func.add_point())
 add_point_button.place(x=640, y=177, width=80, height=27)
 
 ##
@@ -109,8 +107,8 @@ del_point_num_label.place(x=400, y=270)
 del_point_entry=Entry(font='Helvetica')
 del_point_entry.place(x=570, y=267, width=50)
 
-del_point_button=Button(font='Helvetica', text='Удалить')
-del_point_button.place(x=640, y=277, width=80, height=27)
+del_point_button=Button(font='Helvetica', text='Удалить', command= lambda: ui_func.del_point(del_point_entry.get()))
+del_point_button.place(x=640, y=267, width=80, height=27)
 
 ##
 ## Изменение точки
@@ -130,17 +128,18 @@ change_coordinate_label.place(x=400, y=388)
 
 change_point_entry_x=Entry(font='Helvetica')
 change_point_entry_x.place(x=440, y=385, width=50)
+
 change_point_entry_y=Entry(font='Helvetica')
 change_point_entry_y.place(x=550, y=385, width=50)
 
-change_point_button=Button(font='Helvetica', text='Изменить')
+change_point_button=Button(font='Helvetica', text='Изменить', command=lambda: ui_func.change_point(change_point_entry.get()))
 change_point_button.place(x=640, y=385, width=85, height=27)
 
 ##
 ## Очистка множества точек
 ##
 
-clean_button=Button(font='Helvetica 12 bold', text = 'Очистить множество точек', command=lambda: main.clean_all(input_count.get()))
+clean_button=Button(font='Helvetica 12 bold', text = 'Очистить множество точек', command=lambda: ui_func.clean_all())
 clean_button.place(x=400, y=450)
 
 ##
---
+++ lab_01/ui_func.py
@@ -0,0 +1,178 @@
+from multiprocessing import Condition
+import ui
+from tkinter import *
+from tkinter import Canvas, messagebox
+
+from prettytable import PrettyTable
+
+points=[]
+count = 0
+
+## функция для проверки количества точек и изменения статуса полей для ввода
+def check_count(count_input):     
+    try:
+        count_input = int(count_input)
+    except:
+        messagebox.showerror('Ошибка','Количество точек- целое число')
+        ui.input_count.delete(0, 'end')
+        return
+
+    if (count_input < 0):
+        messagebox.showerror('Ошибка','Количество точек- целое число')
+        ui.input_count.delete(0, 'end')    
+    else:
+        if (count_input > 10):
+            messagebox.showinfo('Информация','Введите в таблицу первые 10 точек. Остальные добавляйте по одной.')
+            count_input = 10
+            ui.input_count.delete(0,'end')
+            ui.input_count.insert(0, 10)
+        ui.input_count.config(state='readonly')
+
+        for i in range(count_input):
+            ui.name_entry_x[i].config(state='normal')
+            ui.name_entry_y[i].config(state='normal')
+
+        ui.input_table_button.config(state='normal')
+        ui.count_points_button.config(state='disable')
+
+## функция для считывания точек из таблицы
+def read_points(count_input):
+    count_input = int(count_input)
+    try:
+        for i in range(count_input):
+            float(ui.name_entry_x[i].get())
+            float(ui.name_entry_y[i].get())  
+    except:
+        text='Координаты точек- вещесвтенные числа. Количество точек должно соответствовать ' + str(count_input) + '.'
+        messagebox.showerror('Ошибка',text)
+        return
+    for i in range(count_input):
+        points.append([float(ui.name_entry_x[i].get()), float(ui.name_entry_y[i].get())]) 
+        ui.name_entry_x[i].config(state='readonly')
+        ui.name_entry_y[i].config(state='readonly')
+
+    ui.input_table_button.config(state='disable')
+    global count
+    count += count_input
+
+    print(points)
+
+    
+## функция для добавления точки
+def add_point():
+    try:
+        x=float(ui.add_point_entry_x.get())
+        y=float(ui.add_point_entry_y.get())
+    except:
+        messagebox.showerror('Ошибка','Координаты точки- вещественные числа')
+        return 
+
+    points.append([x,y])
+    global count
+    count += 1
+
+    print(points)
+    print(count)
+
+## функция для печати таблицы
+def print_table():
+
+    table=Tk()
+    table.title('Table')
+    table.geometry('400x600')  
+
+    scroll_bar = Scrollbar(table)
+    scroll_bar.pack(side = RIGHT, fill = Y)
+
+    mytable = PrettyTable()
+    mytable.field_names = [' Номер ', '       X       ', '       Y       ']
+
+    for i in range(count):
+        x = str(points[i][0]) 
+        y = str(points[i][1])
+        mytable.add_row([str(i+1), x, y])
+    
+    tb = Text(table, width=300, height=600, background='light grey')
+    tb.insert(INSERT,mytable)
+    tb.pack()
+    table.mainloop()
+
+## функция для проверки номера точки
+def check_point(number):
+    try:
+        index = int(number)
+    except:
+        messagebox.showerror('Ошибка','Неправильно введён номер точки. Номер точки- целое число.')
+        return 1
+
+    global count
+
+    if (count == 0):
+        messagebox.showinfo('Информация','Множество точек пусто.')
+        return 1
+
+    if (index < 1 or index > count):
+        messagebox.showerror('Ошибка','Неправильно введён номер точки.\
+        Номер точки должен быть целым число и не превосходить количество точек множества.')
+        return 1
+
+    return 0
+
+## фукция для удаления точки
+def del_point(number):
+    if (check_point(number) == 1):
+        return
+    
+    points.pop(int(number) - 1)
+    global count
+    count -= 1
+
+
+## функция для изменения координат точки
+def change_point(number):
+    if (check_point(number) == 1):
+        return
+    
+    x = ui.change_point_entry_x.get()
+    y = ui.change_point_entry_y.get()
+
+    try:
+        num_x = float(x)
+        num_y = float(y)
+    except:
+        messagebox.showerror('Ошибка','Координаты точек- вещесвтенные числа.')
+        return
+
+    points[int(number) - 1] = [num_x, num_y]
+
+## функция для очистки всех полей и массива
+def clean_all():
+    points.clear()
+    global count
+    count = 0
+
+    for i in range(10):
+        ui.name_entry_x[i].config(state='normal')
+        ui.name_entry_x[i].delete(0, 'end')
+        ui.name_entry_x[i].config(state='readonly')
+
+        ui.name_entry_y[i].config(state='normal')
+        ui.name_entry_y[i].delete(0, 'end')
+        ui.name_entry_y[i].config(state='readonly')
+
+    ui.input_table_button.config(state='disable')
+
+    ui.input_count.config(state='normal')
+    ui.input_count.delete(0,'end')
+
+    ui.count_points_button.config(state='normal')
+    
+    ui.add_point_entry_x.delete(0,'end')
+    ui.add_point_entry_y.delete(0,'end')
+
+    ui.change_point_entry_x.delete(0,'end')
+    ui.change_point_entry_y.delete(0,'end')
+    ui.change_point_entry.delete(0,'end')
+    
+    ui.del_point_entry.delete(0,'end')
+    
