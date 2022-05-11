bdb7ba8f52f74f5c74a97e653086f4c855e1c81f
Dashori <daahaaa@icloud.com>
2022-05-11 04:30:57 +0300

    everything works

---
+++ lab_07/draw.py
@@ -0,0 +1,249 @@
+from tkinter import *
+from tkinter import messagebox
+import time
+import ui
+from colormap import rgb2hex
+
+lines = []
+
+flag_draw = 0
+min_x = 0
+max_x = 0
+min_y = 0
+max_y  = 0
+
+rect = None
+old_coor_rect = None
+
+old_coor = None
+
+INVISIBLE = 0
+VISIBLE = 1
+
+## функция для считывания координат х и у из полей ввода
+def input_coor(input_x, input_y):
+    x = input_x.get()
+    y = input_y.get()
+
+    try:
+        x = float(input_x.get())
+        y = float(input_y.get())
+        return x, y
+    except:
+        messagebox.showerror("Ошибка", "Неправильно ввёдены числовые значения.")
+        input_x.delete(0, "end")
+        input_y.delete(0, "end")
+        return 'a', 'b'
+
+
+##функция для считывания отрезка
+def input_line(input_x1, input_y1, input_x2, input_y2):
+    x1, y1 = input_coor(input_x1, input_y1)
+
+    if (x1 == 'a' and y1 == 'b'):
+        return
+
+    x2, y2 = input_coor(input_x2, input_y2) 
+
+    if (x2 == 'a' and y2 == 'b'):
+        return
+
+    add_line(x1, y1, x2, y2)
+
+def add_line(x1, y1, x2, y2):
+    global lines
+
+    lines.append([x1, y1, x2, y2])
+
+    print(lines)
+
+    ui.canv.create_line(x1, y1, x2, y2, fill='orange')
+
+
+## функция для получения координат натыканных точек        
+def input_points(event):
+    global old_coor
+    x = float(event.x)
+    y = float(event.y)
+    
+    if old_coor == None:
+        old_coor=([x,y])
+    else:
+        add_line(old_coor[0], old_coor[1], x, y)
+        old_coor = None
+    
+
+## функция для тыканья отрезка
+def input_points_canvas():
+    ui.canv.bind('<Button - 1>', input_points)
+
+
+## функция для ввода отсекателя с полей ввода
+def input_rect(input_x1, input_y1, input_x2, input_y2):
+    x1, y1 = input_coor(input_x1, input_y1)
+
+    if (x1 == 'a' and y1 == 'b'):
+        return
+
+    x2, y2 = input_coor(input_x2, input_y2) 
+
+    if (x2 == 'a' and y2 == 'b'):
+        return
+
+    global rect
+    if rect != None:
+        ui.canv.create_rectangle(rect[0], rect[1], rect[2], rect[3], outline='white')
+
+    rect = [x1, y1, x2, y2]
+
+    ui.canv.create_rectangle(x1, y1, x2, y2, outline='red')
+    
+
+def input_rect_bind(event):
+    global rect
+    if rect != None:
+        ui.canv.create_rectangle(rect[0], rect[1], rect[2], rect[3], outline='white')
+
+    global old_coor_rect
+
+    x = float(event.x)
+    y = float(event.y)
+    
+    if old_coor_rect == None:
+        old_coor_rect=([x,y])
+    else:
+        rect = [old_coor_rect[0], old_coor_rect[1], x, y]
+        ui.canv.create_rectangle(rect[0], rect[1], rect[2], rect[3], outline='red')
+        old_coor_rect = None
+
+## функция для тыканья отсекателя
+def input_rect_canvas():
+    ui.canv.bind('<Button - 1>', input_rect_bind)
+
+## функция для очистки всего
+def clean():
+    ui.canv.delete("all")
+    global lines, rect
+
+    lines = []
+    rect = None
+
+
+def get_code(x, y):
+    global rect
+
+    code = [0, 0, 0, 0]
+    if x < rect[0]:
+        code[0] = 1
+    if x > rect[1]:
+        code[1] = 1
+    if y < rect[2]:
+        code[2] = 1
+    if y > rect[3]:
+        code[3] = 1
+
+    return code
+
+def log_prod(code1, code2):
+    p = 0
+    for i in range(4):
+        p += code1[i] & code2[i]
+
+    return p
+
+
+#  Видимость
+def is_visible(line):
+    """Видимость - 0 = невидимый
+                   1 = видимый
+                   2 = частично видимый"""
+
+    # вычисление кодов концевых точек отрезка
+    s1 = sum(get_code(line[0], line[1]))
+    s2 = sum(get_code(line[2], line[3]))
+
+    # предположим, что отрезок частично видим
+    vis = 2
+
+    # проверка полной видимости отрезка
+    if not s1 and not s2:
+        vis = VISIBLE
+    else:
+        # проверка тривиальной невидимости отрезка
+        l = log_prod(get_code(line[0], line[1]), get_code(line[2], line[3]))
+        if l != 0:
+            vis = INVISIBLE
+
+    return vis
+
+
+def cutoff_line(line):
+    global rect
+
+    print("IN FUNC", line, rect)
+    # инициализация флага
+    flag = 1 # общего положения
+    t = 1
+
+    # проверка вертикальности и горизонтальности отрезка
+    if abs(line[2] - line[0]) < 1e-6:
+        flag = -1   # вертикальный отрезок
+    else:
+        # вычисление наклона
+        t = (line[3] - line[1]) / (line[2] - line[0])
+        if abs(t) < 1e-6:
+            flag = 0   # горизонтальный
+
+    # для каждой стороны окна
+    for i in range(4):
+        print(" HERE ")
+        vis = is_visible(line)
+        if vis == VISIBLE:
+            ui.canv.create_line(line[0], line[1], line[2], line[3], fill='green')
+            print(" VISIBLE ")
+            return
+        elif vis == INVISIBLE:
+            return
+
+        # проверка пересечения отрезка и стороны окна
+        code1 = get_code(line[0], line[1])
+        code2 = get_code(line[2], line[3])
+
+        if code1[i] == code2[i]:
+            continue
+
+        # проверка нахождения Р1 вне окна; если Р1 внутри окна, то Р2 и Р1 поменять местами
+        if code1[i] == 0:
+            line[0], line[2] = line[2], line[0]
+            line[1], line[3] = line[3], line[1]
+
+        # поиск пересечений отрезка со сторонами окна
+        # контроль вертикальности отрезка
+        if flag != -1:
+            if i < 2:
+                line[1] = t * (rect[i] - line[0]) + line[1]
+                line[0] = rect[i]
+                continue
+            else:
+                line[0] = (1 / t) * (rect[i] - line[1]) + line[0]
+
+        line[1] = rect[i]
+
+    ui.canv.create_line(line[0], line[1], line[2], line[3], fill='green')
+
+
+def cutoff():
+    global lines, rect
+    if rect == None:
+        messagebox.showerror("Ошибка", "Не введен отсекатель")
+        return
+
+    if (len(lines) == 0):
+        messagebox.showerror("Ошибка", "Не введены отрезки")
+        return
+
+    rect = [min(rect[0], rect[2]), max(rect[0], rect[2]), min(rect[1], rect[3]), max(rect[1], rect[3])]
+
+
+    for line in lines:
+        cutoff_line(line)
---
+++ lab_07/ui.py
@@ -0,0 +1,207 @@
+from re import I
+from tkinter import *
+from tkinter.tix import MAIN
+import main, draw
+
+const_bg = '#ffffff'
+const_draw ='#12d9d9'
+const_line = '#000002'
+
+window_size_X = 800
+window_size_Y = 800
+
+width_but = 200
+
+image_canvas = None
+
+window = Tk()
+window.title("Lab 7 Chepigo Darya IU7-44B")
+window.geometry("1400x1000")
+
+
+def config(event):
+    if event.widget == window:
+        global window_size_X, window_size_Y, width_but, image_canvas
+
+        window_size_X = window.winfo_width() / 800
+        window_size_Y = window.winfo_height() / 800
+
+        ## условие
+        task_button.place(x=400 * window_size_X,y=750 * window_size_Y,width=120 * window_size_X,height=30 * window_size_Y)
+
+        ## о программе
+        info_button.place(x=540 * window_size_X,y=750 * window_size_Y,width=120 * window_size_X,height=30 * window_size_Y)
+
+        ## очистить канву
+        clean_canvas_but.place(x=250 * window_size_X,y=750 * window_size_Y,width=120 * window_size_X,height=30 * window_size_Y,)
+
+        ## выход
+        exit_button.place(x=680 * window_size_X,y=750 * window_size_Y,width=100 * window_size_X,height=30 * window_size_Y,)
+
+        width_but = 200 * window_size_X
+        height_but = 30 * window_size_Y
+        x_but = 20 * window_size_X
+
+        add_point_label.place(x=x_but, y=40  * window_size_Y)
+
+        ## добавить отрезок 1 точка
+        coordinate_new_label_x1.place(x=x_but, y=84 * window_size_Y)
+        coordinate_new_label_y1.place(x=130 * window_size_X, y=84 * window_size_Y)
+
+        add_point_entry_x1.place(x=60 * window_size_X, y=80 * window_size_Y, width=50 * window_size_X)
+        add_point_entry_y1.place(x=170 * window_size_X, y=80 * window_size_Y, width=50 * window_size_X)
+
+        ## добавить отрезок 2 точка
+        coordinate_new_label_x2.place(x=x_but, y=134 * window_size_Y)
+        coordinate_new_label_y2.place(x=130 * window_size_X, y=134 * window_size_Y)
+
+        add_point_entry_x2.place(x=60 * window_size_X, y=130 * window_size_Y, width=50 * window_size_X)
+        add_point_entry_y2.place(x=170 * window_size_X, y=130 * window_size_Y, width=50 * window_size_X)
+
+        add_but.place(x=x_but, y=185 * window_size_Y, width=width_but, height=30 * window_size_Y)
+        draw_add_but.place(x=x_but, y=235 * window_size_Y, width=width_but, height=30 * window_size_Y)
+
+        ## добавить отсекатель 1 точка
+
+        add_rect_label.place(x=x_but, y=290 * window_size_Y)
+
+        rect_coordinate_new_label_x1.place(x=x_but, y=324 * window_size_Y)
+        rect_coordinate_new_label_y1.place(x=130 * window_size_X, y=324 * window_size_Y)
+
+        add_rect_entry_x1.place(x=60 * window_size_X, y=320 * window_size_Y, width=50 * window_size_X)
+        add_rect_entry_y1.place(x=170 * window_size_X, y=320 * window_size_Y, width=50 * window_size_X)
+
+        ## добавить отсекатель 2 точка
+        rect_coordinate_new_label_x2.place(x=x_but, y=374  * window_size_Y)
+        rect_coordinate_new_label_y2.place(x=130 * window_size_X, y=374 * window_size_Y)
+
+        add_rect_entry_x2.place(x=60 * window_size_X, y=370 * window_size_Y, width=50 * window_size_X)
+        add_rect_entry_y2.place(x=170 * window_size_X, y=370 * window_size_Y, width=50 * window_size_X)
+
+        add_rect.place(x=x_but, y=425 * window_size_Y, width=width_but, height=30 * window_size_Y)
+        draw_rect_but.place(x=x_but, y=475 * window_size_Y, width=width_but, height=30 * window_size_Y)
+
+        ## отсечь
+        cutoff_but.place(x=x_but, y=520 * window_size_Y, width=width_but, height=height_but)
+
+        ## закрасить
+        # fill_but.place(x=x_but, y=640 * window_size_Y, width=width_but, height=height_but)
+
+        ## цвет
+        # color_bg.place(x=x_but, y=750 * window_size_Y, width=width_but, height=height_but)
+        # color_draw.place(x=x_but, y=690 * window_size_Y, width=width_but, height=height_but)
+
+        canv.place(x=250 * window_size_X,y=40 * window_size_Y, width=530 * window_size_X,height=700 * window_size_Y)
+
+        image_canvas = PhotoImage(width = window.winfo_width(), height = window.winfo_height())
+        canv.create_image((window.winfo_width() / 2, window.winfo_height() / 2), image = image_canvas, state = "normal")
+
+window.bind("<Configure>", config)
+canv = Canvas(window, bg="white")
+
+##
+## добавить отрезок
+##
+
+add_point_label=Label(font='Helvetica 14 bold', text='Введите координаты отрезка:')
+
+coordinate_new_label_x1=Label(font='Helvetica', text='X_1     =')
+coordinate_new_label_y1=Label(font='Helvetica', text='Y_1     =')
+
+add_point_entry_x1=Entry(font='Helvetica')
+add_point_entry_y1=Entry(font='Helvetica')
+
+
+coordinate_new_label_x2=Label(font='Helvetica', text='X_2     =')
+coordinate_new_label_y2=Label(font='Helvetica', text='Y_2     =')
+
+add_point_entry_x2=Entry(font='Helvetica')
+add_point_entry_y2=Entry(font='Helvetica')
+
+add_but = Button(text="Добавить отрезок", font="Helvetica 13 bold",
+command= lambda: draw.input_line(add_point_entry_x1, add_point_entry_y1, add_point_entry_x2, add_point_entry_y2 ))
+draw_add_but = Button(text="Нарисовать отрезок", font="Helvetica 13 bold", command= lambda: draw.input_points_canvas())
+
+
+##
+## добавить отсекатель
+##
+
+add_rect_label=Label(font='Helvetica 14 bold', text='Введите координаты отсекателя:')
+
+rect_coordinate_new_label_x1=Label(font='Helvetica', text='X_1     =')
+rect_coordinate_new_label_y1=Label(font='Helvetica', text='Y_1     =')
+
+add_rect_entry_x1=Entry(font='Helvetica')
+add_rect_entry_y1=Entry(font='Helvetica')
+
+
+rect_coordinate_new_label_x2=Label(font='Helvetica', text='X_2     =')
+rect_coordinate_new_label_y2=Label(font='Helvetica', text='Y_2     =')
+
+add_rect_entry_x2=Entry(font='Helvetica')
+add_rect_entry_y2=Entry(font='Helvetica')
+
+
+add_rect = Button(text='Добавить отсекатель', font="Helvetica 13 bold", 
+command= lambda: draw.input_rect(add_rect_entry_x1, add_rect_entry_y1, add_rect_entry_x2, add_rect_entry_y2))
+draw_rect_but = Button(text="Нарисовать отсекатель", font="Helvetica 13 bold", command= lambda: draw.input_rect_canvas())
+
+
+##
+## отсечь 
+##
+
+cutoff_but = Button(text='Отсечь', font="Helvetica 13 bold", command= lambda: draw.cutoff())
+
+##
+## замкнуть
+##
+
+lock_but = Button(text="Замкнуть", font="Helvetica 14 bold", command= lambda: draw.lock_edge())
+
+##
+## закрасить
+##
+
+fill_but = Button(text="Закрасить", font="Helvetica 14 bold", command = lambda: draw.fill_figure())
+
+##
+## Выбрать цвет фонa
+##
+
+color_bg = Button(text="Выбрать цвет фона", font="Helvetica 14 bold", command=lambda: main.change_bg())
+
+
+##
+## Выбрать цвет заполнения
+##
+
+color_draw = Button(text="Выбрать цвет для заполнения",font="Helvetica 14 bold",command=lambda: main.change_draw())
+
+##
+## Очистить канвас
+##
+
+clean_canvas_but = Button(font="Helvetica 14 bold", text="Очистить канвас", command=lambda: draw.clean())
+
+##
+## Инфо и условие
+##
+
+task_button = Button(font="Helvetica 14 bold", text="Условие", command=lambda: main.task_programm())
+
+info_button = Button(font="Helvetica 14 bold", text="О программе", command=lambda: main.info_programm())
+
+
+##
+## Выход
+##
+
+exit_button = Button(font="Helvetica 14 bold", text="Выход", command=lambda: window.destroy())
+
+
+
+
+draw.input_points_canvas()
+window.mainloop()
